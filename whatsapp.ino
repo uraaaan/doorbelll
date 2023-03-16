481c3e9926dc3acaebb1c5145e7be14dfac8fcda
uraaaan <128032867+uraaaan@users.noreply.github.com>
2023-03-16 09:59:07 +0100

    Add files via upload
    my first project

---
+++ whatsapp.ino
@@ -0,0 +1,37 @@
+#include <WiFi.h>    
+#include <HTTPClient.h>
+#include <UrlEncode.h>
+//int sensor= 13;
+
+
+
+//const char* ssid = "dammy";
+//const char* password = "uranus1234";
+
+String phoneNumber = "+2348138284021";
+String apiKey = "4012333";
+
+void sendMessage(String message){
+
+  // Data to send with HTTP POST
+  String url = "https://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);    
+  HTTPClient http;
+  http.begin(url);
+
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(url);
+  if (httpResponseCode == 200){
+    Serial.print("Message sent successfully");
+  }
+  else{
+    Serial.println("Error sending the message");
+    Serial.print("HTTP response code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Free resources
+  http.end();
+}
